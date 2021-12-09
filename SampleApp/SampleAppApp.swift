d7c2bfd0db4d11cf3c0ac88b9b2f28405fcb9781
jayshah123 <jayshah67@gmail.com>
2021-12-09 23:10:27 +0530

    Initial Commit

---
+++ SampleApp/SampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppApp.swift
+//  SampleApp
+//
+//  Created by jay shah on 09/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
